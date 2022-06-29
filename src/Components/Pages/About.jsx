5e2e30f9e735ecdf2867ac4a8d124ab422b41606
Millie Cheung <cheungmillie9@gmail.com>
2022-06-29 16:06:47 +0200

    installed styled.components and added arrow animation

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -1,4 +1,7 @@
+import KeyboardDoubleArrowDownRoundedIcon from "@mui/icons-material/KeyboardDoubleArrowDownRounded";
 import { Box, Container, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import styled, { keyframes } from "styled-components";
 import { useTheme } from "../Contexts/ThemeContextProvider";
 import Coding from "./About-elements/Coding";
 import Cruiselines from "./About-elements/Cruiselines";
@@ -12,6 +15,22 @@ import Travel from "./About-elements/Travel";
 
 const About = () => {
   const { theme } = useTheme();
+  const [maskTop, setMaskTop] = useState(34);
+
+  useEffect(() => {
+    const adjustMaskTop = () => {
+      console.log(window.scrollY);
+      let y = window.scrollY;
+      y < 11.5
+        ? setMaskTop(34)
+        : y <= 400
+        ? setMaskTop(y * 1.45)
+        : setMaskTop(y * 1.4);
+    };
+    window.addEventListener("scroll", adjustMaskTop, { passive: true });
+    return () => window.removeEventListener("scroll", adjustMaskTop);
+  });
+
   return (
     <Container sx={{ py: "4rem", background: "#ECF1F5" }}>
       {/* background color for header */}
@@ -26,27 +45,45 @@ const About = () => {
           height: "4rem",
         }}
       />
-
       {/* main content  */}
-      <Typography variant="h4" theme={theme} textAlign="center">
-        ABOUT ME
-      </Typography>
-      <Typography
-        variant="h6"
-        theme={theme}
-        textAlign="center"
-        paddingBottom="5rem"
-      >
-        Scroll down to see my life journey!
-      </Typography>
+      <Box sx={{ textAlign: "center" }}>
+        <Typography variant="h4" theme={theme} textAlign="center">
+          ABOUT ME
+        </Typography>
+        <Typography variant="h6" theme={theme} textAlign="center">
+          Scroll down to see my life journey!
+        </Typography>
+        {/* <DownArrow fontSize="large" sx={{ pt: "1rem", pb: "5rem" }} /> */}
+        <DownArrow>
+          <KeyboardDoubleArrowDownRoundedIcon
+            color="#3c3c3c"
+            fontSize="large"
+            sx={{ pt: "1rem", pb: "5rem" }}
+          />
+        </DownArrow>
+      </Box>
 
       <Box
         sx={{
           display: "flex",
           flexDirection: "row",
           py: "20px",
+          position: "relative",
         }}
       >
+        <Box
+          sx={{
+            background: "#ECF1F5",
+            position: "absolute",
+            top: maskTop,
+            bottom: 0,
+            left: -10,
+            right: 0,
+            zIndex: 98,
+            scrollBehavior: "smooth",
+          }}
+        />
+
         {/* left box */}
         <Box sx={{ width: { xs: "30%", sm: "35%", md: "50%" } }} />
 
@@ -73,5 +110,15 @@ const About = () => {
     </Container>
   );
 };
+
+const bouncingArrow = keyframes`
+  0%, 100%  { transform: translateY(0); }
+  25%, 75% { transform: translateY(-10px); }
+  50%, { transform: translateY(-20px); }
+  `;
+
+const DownArrow = styled.h1`
+  animation: ${bouncingArrow} 1.5s infinite;
+`;
 
 export default About;
