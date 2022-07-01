2f82735fef37c04b0e5331e60d70ff67b2e63651
Millie Cheung <cheungmillie9@gmail.com>
2022-07-01 18:31:42 +0200

    note to myself: displayElement function should be using the element's height to get the top instead of a "number"

--- src/Helper.js
+++ src/Helper.js
@@ -1,4 +1,22 @@
 export const displayElement = (yPos) => {
+  console.log(window.scrollY);
   if (window.scrollY > yPos) return true;
   return false;
 };
+
+// export const displayElement = (elementId) => {
+//   const main = document.getElementById("main");
+//   const prevEl = document.getElementById("jobs-pt");
+//   const element = document.getElementById(elementId);
+//   //const element = document.getElementById("jobs-pt");
+
+//   if (!element) return;
+
+//   const yPos =
+//     element.getBoundingClientRect().top - main.getBoundingClientRect().top;
+//   // console.log("yPos " + yPos);
+//   console.log("scrollY " + window.scrollY);
+
+//   if (window.scrollY >= yPos) return true;
+//   return false;
+// };
