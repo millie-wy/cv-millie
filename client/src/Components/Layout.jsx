d514a517388b4e94e0a418c83120dd886c1ba567
Millie Cheung <cheungmillie9@gmail.com>
2022-08-01 17:13:45 +0200

    page scroll to top when reloaded

--- client/src/Components/Layout.jsx
+++ client/src/Components/Layout.jsx
@@ -2,9 +2,13 @@ import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
+import { useEffect } from "react";
 
 const Layout = () => {
-  window.scrollTo(0, 0);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <Box sx={{ position: "relative" }}>
       <Header />
