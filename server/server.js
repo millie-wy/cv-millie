06ee377d828a7807ca99158e2d3ce97904cc24d1
Millie Cheung <cheungmillie9@gmail.com>
2022-08-03 12:21:05 +0200

    server config for heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,16 +1,27 @@
+import cookieSession from "cookie-session";
 import express from "express";
 import mongoose from "mongoose";
+import { adminRouter } from "./resources/admin/index.js";
 import { mediaRouter } from "./resources/media/index.js";
 import { projectRouter } from "./resources/project/index.js";
-import { adminRouter } from "./resources/admin/index.js";
-import cookieSession from "cookie-session";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import "dotenv/config";
 
 const app = express();
-const uri = process.env.MONGODB_URI;
-const port = process.env.PORT || 8000;
+app.use(cors);
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB has been connected"))
+  .catch((err) => console.log(err));
 
 // global middlewares
-app.use(express.json());
+// app.use(express.json());
 app.use(
   cookieSession({
     secret: "49eFKEQh32E3grDJI",
@@ -25,11 +36,15 @@ app.use("/api/admin", adminRouter);
 app.use("/api/project", projectRouter);
 app.use("/api/media", mediaRouter);
 
-mongoose.connect(`"${uri}"`, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Connection to database established.");
-    app.listen(port, () => console.log("Server is running on port: " + port));
-  }
+const PORT = process.env.PORT || 3001;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("Server is now running on port " + PORT);
 });
