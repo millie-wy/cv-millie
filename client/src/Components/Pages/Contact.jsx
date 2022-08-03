d2c04f3251a92175be26cc7921b341a81dffeab7
Millie Cheung <cheungmillie9@gmail.com>
2022-08-03 18:53:37 +0200

    page setup

--- client/src/Components/Pages/Contact.jsx
+++ client/src/Components/Pages/Contact.jsx
@@ -1,5 +1,40 @@
+import { Box, Container, Typography } from "@mui/material";
+import { useTheme } from "../Contexts/ThemeContextProvider";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const { theme } = useTheme();
+
+  return (
+    <Container
+      maxWidth={false}
+      sx={{
+        py: "4rem",
+        background: "#ECF1F5",
+        height: "fit-content",
+        minHeight: "calc(100vh - 3.5rem)",
+        overflow: "hidden",
+        scrollBehavior: "smooth",
+      }}
+    >
+      {/* background color for header */}
+      <Box
+        sx={{
+          background: "#ECF1F5",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: -99,
+          height: "4rem",
+        }}
+      />
+
+      {/* subject */}
+      <Typography variant="h4" theme={theme} textAlign="center">
+        CONTACT ME
+      </Typography>
+    </Container>
+  );
 };
 
 export default Contact;
